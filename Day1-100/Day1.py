a6a1a3f96c2224cf4549e2c0fc6b5bfdda43e948
Harsh Bhatt <harshbhatt0151@gmail.com>
2022-12-12 22:44:05 +0530

    day01

---
+++ Day1-100/Day1.py
@@ -0,0 +1,62 @@
+#simple print statement
+
+print("harsh bhatt a blockchain developer who is learning python now")
+
+#-------------------------------------------------------------------------------
+
+
+# print statement with multiple lines
+
+print(" harsh bhatt\n is a blockchain developer\nprint('n at nest')")
+
+#--------------------------------------------------------------------------------
+
+
+#concatenation of strigs in print statememt
+
+print("harsh" + " bhatt" + " a good person")
+
+#--------------------------------------------------------------------------------
+
+
+# miscallaneous examples of print statements
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('"e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#--------------------------------------------------------------------------------
+
+# INPUT statements concatenation
+
+a = input("what is your name: ")
+b = input("what is your surname: ")
+c = a + b
+print(a + " " + b)
+
+#---------------------------------------------------------------------------------
+
+#measuring length of the string
+
+ab = len("harshbhatt")
+print(ab)
+
+#--------------------------------------------------------------------------------
+
+# storing variables and reversing the memory
+
+a = input("what is your name?")
+b = input("what is your surname?")
+
+c = a # variable c takes value of a
+a = b # now value of b gies in a 
+b = c # and value of a stired in c, is taken by variable b
+
+print("a: "+ a)
+print("b: "+ b)
+
+#-------------------------------------------------------------------------------------
+
+
+
